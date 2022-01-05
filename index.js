63ec956a616781d89212031e6a84077a1c33a5b3
Anthony Geritano <anthony.geritano@aya.yale.edu>
2022-01-04 22:00:09 -0500

    added most prompts

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
 console.log('hello node');
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
+// const generateHTML = require('./src/generateHTML');
+
+console.log(`
+=================
+Add information 
+for new employees 
+in prompts below
+================`);
+
+const promptToAddEmployee = () => {
+    return inquirer.prompt([
+    {
+        type: 'confirm',
+        name: 'addNewEmployee',
+        message: 'Would you like to add a new employee',
+        default: false
+    }
+])};
+
+const addEmployeeInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What role will the new employee',
+            choices: ['Engineer', 'Intern', 'Manager'] 
+        },
+        {
+            type:'input',
+            name: 'employeeName',
+            message: "What is the employee's name?",
+            validate: EmployeeNameInput => {
+                if (EmployeeNameInput){
+                  return true;
+                } else {
+                  console.log ("Please enter your employee's name!");
+                  return false;
+                }
+              }
+        },
+        {
+            type:'input',
+            name:'employeeEmail',
+            message:"What is the employee's email",
+            validate: EmployeeEmailInput => {
+                if(EmployeeEmailInput){
+                    return true;
+                } else{
+                    console.log("Please enter the Employee's Email!")
+                    return false;
+                }
+            }
+        },
+        {
+            type:'number',
+            name:'employeeIdNumber',
+            message:'What is the employee ID number?',
+        },
+    ])
+};
+
+promptToAddEmployee().then(addEmployeeInfo)
